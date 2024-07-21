08ebd9aa6ed43cf249acbfaf75b520ecbb3e8f91
vzbel <142253332+vzbel@users.noreply.github.com>
2024-07-21 12:45:27 -0600

    Begin menu class implementation

--- Menu/Menu.java
+++ Menu/Menu.java
@@ -3,12 +3,63 @@ package Menu;
 import java.util.ArrayList;
 import java.io.File;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Menu {
-    private File mealsFile = new File("./Meals.csv");
-    private ArrayList<String> meals;
-    private Scanner scanner;
+    private File mealsFile; // File contains all menu meals
+    private Scanner scanner; // Used to scan meal files
+    private ArrayList<Meal> mealsList; // List containing all meals
 
-    public Menu() {
+    // Create menu based off of a single meals file
+    public Menu(File mealsFile) {
+        this.mealsFile = mealsFile;
+        this.mealsList = new ArrayList<Meal>(); // Create new list of meals.
+        // Import all meals
+        this.importMeals(this.mealsFile);
+    }
+
+    // Import meals from a file
+    public void importMeals(File mealsFile) {
+        try {
+            // Create scanner for meals file
+            scanner = new Scanner(mealsFile);
+            // Read file
+            while (scanner.hasNextLine()) {
+                // Split line into meaningful sections (csv format)
+                String[] mealProperties = scanner.nextLine().split(",");
+
+                // Denote meal properties
+                String mealName = mealProperties[0];
+                String mealSize = mealProperties[1];
+                String mealCalories = mealProperties[2].split(" ")[0]; // remove "Cal" from property (check csv)
+                double mealPrice = Double.parseDouble(mealProperties[3]); // convert price from String to double
+
+                // Create meal object based off of each property
+                Meal meal = new Meal(mealName, mealSize, mealCalories, mealPrice);
+
+                // Insert meal object into the meals list.
+                mealsList.add(meal);
+            }
+        } catch (Exception e) {
+            System.out.println("Meals @importMeals: " + e);
+        }
+    }
+
+    // Get menu as String
+    public String toString() {
+        if (mealsList.isEmpty()) {
+            return "There are no meals to display.";
+        }
+        // String to which we will append each meal
+        String menuString = "---- Meals ----\n";
+
+        // Iterate through mealslist
+        for (Meal meal : mealsList) {
+            // Append meal to string
+            menuString += meal.getName() + ", $" + meal.getPrice() + "\n";
+        }
+
+        // Return result
+        return menuString.trim() + "\n---- Meals ----";
     }
 }
