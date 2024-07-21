2e5c431df1db4feec1e2facc845b67158a053805
vzbel <142253332+vzbel@users.noreply.github.com>
2024-07-21 11:04:33 -0600

    Implement circular queue class

---
+++ Queue/CircularQueue.java
@@ -0,0 +1,127 @@
+package Queue;
+
+import java.util.Arrays;
+
+// Circular Queue
+public class CircularQueue<E> implements Queue<E> {
+    private E[] array; // Array holding queue values.
+    private int front; // Front position
+    private int rear; // Rear position
+    private int maxSize; // Maximum queue size
+
+    // Create a queue with a given size
+    @SuppressWarnings("unchecked")
+    public CircularQueue(int size) {
+        // Allocate extra space to differentiate empty queues from full queues.
+        // (rear has n possible positions, but the queue has n + 1 possible states.)
+        this.array = (E[]) new Object[size + 1];
+
+        // Place rear at the allocated extra space
+        this.rear = 0;
+        // Place front at first real position.
+        this.front = 1;
+
+        // maxSize accurately reflects the maximum size of the array.
+        this.maxSize = size + 1;
+    }
+
+    // Clear the queue
+    public void clear() {
+        // Reinitialize rear and front
+        this.rear = 0;
+        this.front = 1;
+    }
+
+    // Insert element at rear
+    public boolean enqueue(E element) {
+        // If queue is full (i.e rear is exactly two circular positions behind front)
+        if ((this.rear + 2) % this.maxSize == this.front) {
+            return false;
+        }
+
+        // Circular increment rear
+        this.rear = (this.rear + 1) % this.maxSize;
+
+        // Insert value into array at rear position
+        this.array[this.rear] = element;
+
+        return true;
+    }
+
+    // Remove and return front element
+    public E dequeue() {
+        // If queue is empty (i.e rear is one circular position behind front)
+        if (this.length() == 0) {
+            return null;
+        }
+
+        // Store front element
+        E frontElement = this.array[this.front];
+
+        // Remove front element
+        this.array[this.front] = null;
+
+        // Circular increment front.
+        this.front = (this.front + 1) % this.maxSize;
+
+        // Return front element
+        return frontElement;
+    }
+
+    // Return front element
+    public E peek() {
+        // If queue is empty, no element can be returned.
+        if (this.length() == 0) {
+            return null;
+        }
+        // Access and retrieve front element
+        return this.array[this.front];
+    }
+
+    // Return length
+    public int length() {
+        return ((this.rear + this.maxSize) - this.front + 1) % this.maxSize;
+    }
+
+    // Check if empty
+    public boolean isEmpty() {
+        // Check if rear is behind front
+        return (this.rear + 1) % this.maxSize == this.front; // @ Modified from OG
+    }
+
+    // Return queue contents @ Custom Method
+    public String toString() {
+        // If queue is empty
+        if (this.isEmpty()) {
+            return "The queue is empty.";
+        }
+        // String which we will append queue elements to
+        String queueContents = "F [";
+
+        // Iterate through queue array in a circular manner.
+        int currentPosition = this.front;
+        int appendCounter = 0;
+
+        // Continue to append until the
+        // full number of elements in the queue have been addressed.
+        // Since appendCounter starts from zero, the last element is appended when
+        // appendCounter == this.length() - 1.
+        while (appendCounter < this.length()) {
+            // Check if we are on the last append
+            if (appendCounter == this.length() - 1) {
+                // Use currentPosition to access the current element
+                queueContents += this.array[currentPosition] + "";
+            } else {
+                // Use currentPosition to access the current element
+                queueContents += this.array[currentPosition] + ", ";
+            }
+            // Increment appendCounter
+            appendCounter++;
+            // Circular increment currentPosition
+            currentPosition = (currentPosition + 1) % this.maxSize;
+        }
+
+        // Return contents
+        return queueContents + "] R";
+    }
+}
